19bb7e1ca9bd6240c4ca1fcc0f490060eb6e5856
Shyam <m.shyam.tnj@gmail.com>
2019-08-01 17:40:19 +0530

    Add php code

---
+++ api/upload_shawl.php
@@ -0,0 +1,34 @@
+<?php
+    $server_name = "localhost";
+    $username = "test_masha";
+    $password = "test";
+    $db = "test_masha";
+
+    $conn = new mysqli($server_name, $username, $password, $db);
+    if ($conn->connect_error) {
+        echo "Connection error";
+    }
+    else {
+        $cols = "";
+        $vals = "";
+
+        for ($x = 0; $x < count($_POST); $x++) {
+            $cols = $cols . array_keys($_POST)[$x];
+            $vals = $vals . "'" . array_values($_POST)[$x] . "'";
+            if ($x < count($_POST) - 1) {
+                $cols = $cols . ", ";
+                $vals = $vals . ", ";
+            }
+        }
+
+        $query = "INSERT INTO shawls (" . $cols . ") VALUES (" . $vals . ");";
+
+        if ($conn->query($query) !== TRUE) {
+            http_response_code(400);
+            echo $conn->error;
+        }
+        else {
+            echo "SUCCESS";
+        }
+    }
+?>
