caf3c068f168be0027b403bd2793a8cae2050f56
Shyam <m.shyam.tnj@gmail.com>
2019-08-07 01:36:09 +0530

    fix

--- api/enquiry.php
+++ api/enquiry.php
@@ -2,11 +2,9 @@
     include "auth.php";
     include "tools.php";
 
-    $result = $sql->query("INSERT INTO enquiries () VALUES ();");
-    $name = ($_POST["name"]);
-    $emailid = ($_POST["emailid"]);
-    $queries = ($_POST["queries"]);
-
+    $name = htmlentities($_POST["name"]);
+    $emailid = htmlentities($_POST["emailid"]);
+    $queries = htmlentities($_POST["queries"]);
 
     $result = $sql->query("INSERT INTO enquiries (name, emailid, queries) VALUES ('". $name . "','" . $emailid . "','" . $queries . "');");
 ?>
