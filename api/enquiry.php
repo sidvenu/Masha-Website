87bb54d5270ab91a51bffa73c2e30a89dad85fe9
Shyam <m.shyam.tnj@gmail.com>
2019-08-07 01:33:50 +0530

    fix null

--- api/enquiry.php
+++ api/enquiry.php
@@ -3,9 +3,10 @@
     include "tools.php";
 
     $result = $sql->query("INSERT INTO enquiries () VALUES ();");
-    $name = htmlentities($_POST["name"]);
-    $emailid = htmlentities($_POST["emailid"]);
-    $queries = htmlentities($_POST["queries"]);
+    $name = ($_POST["name"]);
+    $emailid = ($_POST["emailid"]);
+    $queries = ($_POST["queries"]);
+
 
     $result = $sql->query("INSERT INTO enquiries (name, emailid, queries) VALUES ('". $name . "','" . $emailid . "','" . $queries . "');");
 ?>
