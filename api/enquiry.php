4a6d3169bdcdc160b90a5c7e1427759ae4256e80
Shyam <m.shyam.tnj@gmail.com>
2019-08-07 01:29:46 +0530

    implement enquiry storage

--- api/enquiry.php
+++ api/enquiry.php
@@ -2,7 +2,10 @@
     include "auth.php";
     include "tools.php";
 
-    echo $_POST["emailid"];
-    echo $_POST["queries"];
-    echo $_POST["name"];
+    $result = $sql->query("INSERT INTO enquiries () VALUES ();");
+    $name = htmlentities($_POST["name"]);
+    $emailid = htmlentities($_POST["emailid"]);
+    $queries = htmlentities($_POST["queries"]);
+
+    $result = $sql->query("INSERT INTO enquiries (name, emailid, queries) VALUES ('". $name . "','" . $emailid . "','" . $queries . "');");
 ?>
