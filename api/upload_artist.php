f29b267c11c2a612deac5065326ea9bf0d8b25cb
Shyam <m.shyam.tnj@gmail.com>
2019-08-07 18:31:10 +0530

    implement: events api implement: upload artist api

---
+++ api/upload_artist.php
@@ -0,0 +1,31 @@
+<?php
+    include "auth.php";
+
+    $conn = new mysqli($server_name, $username, $password, $db);
+    if ($conn->connect_error) {
+        echo "Connection error";
+    }
+    else {
+        $cols = "";
+        $vals = "";
+
+        for ($x = 0; $x < count($_POST); $x++) {
+            $cols = $cols . array_keys($_POST)[$x];
+            $vals = $vals . "'" . array_values($_POST)[$x] . "'";
+            if ($x < count($_POST) - 1) {
+                $cols = $cols . ", ";
+                $vals = $vals . ", ";
+            }
+        }
+
+        $query = "INSERT INTO artists (" . $cols . ") VALUES (" . $vals . ");";
+
+        if ($conn->query($query) !== TRUE) {
+            http_response_code(400);
+            echo $conn->error;
+        }
+        else {
+            echo "SUCCESS";
+        }
+    }
+?>
