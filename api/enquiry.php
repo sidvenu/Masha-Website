79ce3f8c35b3cdbc44f1859033fbd2e955c63524
Shyam <m.shyam.tnj@gmail.com>
2019-08-07 18:11:13 +0530

    ts: recreate sql connection

--- api/enquiry.php
+++ api/enquiry.php
@@ -2,18 +2,20 @@
     include "auth.php";
     include "tools.php";
 
+    $conn = new mysqli($server_name, $username, $password, $db);
+
     $name = htmlentities($_POST["name"]);
     $emailid = htmlentities($_POST["emailid"]);
     $queries = htmlentities($_POST["queries"]);
 
     $query = "INSERT INTO enquiries (name, emailid, queries) VALUES ('". $name . "','" . $emailid . "','" . $queries . "');";
-    $result = $sql->query($query);
+    $result = $conn->query($query);
     
     echo $query;
     
-    if ($result == TRUE) 
+    if ($result === TRUE) 
         echo "ok";
     else
         header("status-code: 400");
-        echo $sql->error;
+        echo $conn->error;
 ?>
