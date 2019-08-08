419b1c48c7b69b704782687d24aede0c68927427
Shyam <m.shyam.tnj@gmail.com>
2019-08-08 22:02:56 +0530

    implement uploading database

--- api/mashaart_database.py
+++ api/mashaart_database.py
@@ -2,19 +2,33 @@ import sys
 import csv
 import requests
 
-BASE_URL = "http://mashaart.in/api"
-INSERT_RECORD_URLS = {
-    "painting": "/upload_painting.php",
-    "shawl": "/upload_shawl.php",
-    "event": "/upload_event.php",
-    "sculpture": "/upload_sculpture.php",
-    "carpet": "/upload_carpet.php",
-    "exhibition": "/upload_exhibition.php",
-    "event-gallery": "/upload_event_gallery.php",
-    "exhibition-gallery": "/upload_exhibition_gallery.php",
-    "artist": "/artist.php"
-}
+#class to fetch URL
+class URL:
+    BASE_URL = "http://mashaart.in/api"
+    INSERT_RECORD_URLS = {
+        "painting": "/upload_painting.php",
+        "shawl": "/upload_shawl.php",
+        "event": "/upload_event.php",
+        "sculpture": "/upload_sculpture.php",
+        "carpet": "/upload_carpet.php",
+        "exhibition": "/upload_exhibition.php",
+        "event-gallery": "/upload_event_gallery.php",
+        "exhibition-gallery": "/upload_exhibition_gallery.php",
+        "artist": "/upload_artist.php",
+        "inmedia": "/upload_inmedia.php"
+    }
+    
+    @staticmethod
+    def insertURL(key):
+        return URL.BASE_URL + URL.INSERT_RECORD_URLS.get(key)
+    
+    @staticmethod
+    def insertOptions():
+        return URL.INSERT_RECORD_URLS.keys()
 
+#if an argument is provided to the app then change the base url
+if len(sys.argv) > 1: 
+    URL.BASE_URL = sys.argv[1]
 
 #INSERT RECORDS TO THE DATABASE
 def insertDatabase(csvfilename, url):
@@ -38,3 +52,34 @@ def insertDatabase(csvfilename, url):
                 else:
                     print ("ERROR: ")
                     print (response.text)
+
+#App - UI
+class App:
+    def menu(self):
+        print ("1 - INSERT RECORDS")
+        choice = input("\nEnter your choice:")
+
+        if choice == "1":
+            print ("CATEGORIES:")
+            c = 1
+            for x in URL.insertOptions():
+                print(c, "-", x)
+                c+=1
+            c = int(input ("Enter category: "))
+            self.csv = input("Enter csv file:")
+
+            print (self.csv)
+            self.url = ""
+            s = 1
+            for x in URL.insertOptions():
+                if (s == c):
+                    self.url = URL.insertURL(x)
+                    break
+                s += 1
+            
+            print ("uploading to ", self.url)
+            insertDatabase(self.csv, self.url)
+
+#Run the app
+app = App()
+app.menu()
