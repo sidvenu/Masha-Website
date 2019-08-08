950625501b35e0886d53c5b3e2cf356285a60aab
Shyam <m.shyam.tnj@gmail.com>
2019-08-08 22:16:49 +0530

    implement delete API

---
+++ api/delete.php
@@ -0,0 +1,22 @@
+<?php
+    include "auth.php";
+    include "tools.php";
+
+    $id = $_POST["id"];
+
+    $query = [
+        "DELETE FROM paintings WHERE item_number = '{id}'",
+        "DELETE FROM carpets WHERE item_number = '{id}'",
+        "DELETE FROM shawls WHERE item_number = '{id}'",
+        "DELETE FROM sculptures WHERE item_number = '{id}'",
+        "DELETE FROM events WHERE event_id = '{id}'",
+        "DELETE FROM event_gallery WHERE event_id = '{id}'",
+        "DELETE FROM exhibtions WHERE exhibition_id = '{id}'",
+        "DELETE FROM exhibition_gallery WHERE exhibition_id = '{id}'",
+        "DELETE FROM inmedia WHERE id = '{id}'"
+    ];
+
+    for ($x = 0; $x < count($query); $x++) {
+        $sql->query(str_replace("{id}", $id, $query[$x]));
+    }
+?>
