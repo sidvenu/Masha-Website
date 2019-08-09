19e8914c5be0b4e7370e850dbaf72701d53a9106
Shyam <m.shyam.tnj@gmail.com>
2019-08-09 19:53:23 +0530

    implement update_record: query

---
+++ api/update_record.php
@@ -0,0 +1,47 @@
+<?php 
+    include "auth.php";
+    include "tools.php";
+
+    function lookupIdColumn($table) {
+        switch ($table) {
+            case "events":
+            case "event_gallery":
+                return "event_id";
+
+            case "exhibitions":
+            case "exhibition_gallery":
+                return "exhibition_id";
+
+            case "artists":
+                return "artist";
+
+            case "enquiries":
+            case "inmedia":
+                return "id";
+
+            default:
+                return "item_number";
+        }
+    }
+
+    $table = extractFromArray($_POST, "table");
+    $pri_key = lookupIdColumn($table);
+    $id = extractFromArray($_POST, $pri_key);
+
+    $query = "UPDATE " . $table . " SET ";
+
+    //all updates
+    for ($x = 0; $x < count($_POST); $x++) {
+        $col = array_keys($_POST)[$x];
+        $val = $_POST[$col];
+
+        $query .= $col . " = '" . $val . "'";
+
+        if ($x < count($_POST) - 1)
+            $query .= ", ";
+    }
+
+    $query .= " WHERE " . $pri_key . " = '" . $id . "';";
+
+    echo $query;
+?>
