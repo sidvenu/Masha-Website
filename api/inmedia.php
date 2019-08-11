57370a847e10fac52f37fc1354d81f447f164b19
Shyam <m.shyam.tnj@gmail.com>
2019-08-11 20:20:12 +0530

    include tools in some api files

--- api/inmedia.php
+++ api/inmedia.php
@@ -1,5 +1,4 @@
 <?php
-    header("Content-Type: application/json;");
     include "auth.php";
     include "tools.php";
 
@@ -8,76 +7,15 @@
         echo "Connection error";
     }
     else {
-        $where = FALSE;
-        $num = FALSE;
-        $offset = FALSE;
-        $sort = getSortClause($_GET);
-        if (array_search("num", array_keys($_GET)) !== FALSE) {
-            $num = $_GET["num"];
-            array_splice($_GET, array_search("num", array_keys($_GET)), 1);
+        $output = executeGetQuery($sql, "inmedia", ["thumbnail"]);
+
+        if ($output === FALSE) {
+            http_response_code(500);
+            echo "ERROR";
         }
-
-        if (array_search("offset", array_keys($_GET)) !== FALSE) {
-            $offset = $_GET["offset"];
-            array_splice($_GET, array_search("offset", array_keys($_GET)), 1);
+        else {
+            header("Content-Type: application/json;");
+            echo json_encode($output);
         }
-
-        $searchQuery = FALSE;
-        if (array_search("q", array_keys($_GET)) !== FALSE) {
-            $searchQuery = $_GET["q"];
-            array_splice($_GET, array_search("q", array_keys($_GET)), 1);
-        }
-        
-        if ($offset === FALSE) {
-            $offset = 0;
-        } 
-
-        $limit = FALSE;
-
-        if ($num !== FALSE) {
-            $limit = " LIMIT " . $offset . ", " . $num;
-        }
-
-        for ($x = 0; $x < count($_GET); $x++) {
-            $col = array_keys($_GET)[$x];
-            if ($where === FALSE) {
-                $where = " WHERE " . $col . " regexp '" . $_GET[$col] . ".*'";
-            }
-            else {
-                $where = $where . ", " . $col . " = '" . $_GET[$col] . ".*'";
-            }
-        }
-
-        $query = "SELECT * FROM inmedia";
-        $search = "";
-        if ($where !== FALSE) 
-            $query = $query . $where;
-
-        if ($searchQuery !== FALSE && $searchQuery != "") {
-            if ($where === FALSE) {
-                $search = " WHERE ";
-            }
-            else {
-                $search = " AND ";
-            }
-
-            $search .= getSearchClause($searchQuery, ["artist", "medium", "name"]);
-        }
-
-        $query .= $search;
-        $query .= $sort;
-
-        if ($limit !== FALSE) 
-            $query = $query . $limit;
-
-        $query = $query . ";";
-
-        $result = $conn->query($query);
-        $output = array();
-        while ($row = $result->fetch_assoc()) {
-            array_push($output, $row);
-        }
-        echo json_encode($output);
     }   
-
 ?>
