cce1c12053f148823a94726ce2668cfcbc6ecd17
Shyam <m.shyam.tnj@gmail.com>
2019-08-13 01:11:46 +0530

    implement right button for custom slider

--- js/main.js
+++ js/main.js
@@ -349,7 +349,7 @@ jQuery(document).ready(function ($) {
 				globalThis.gallerySliderMaxWidth += $(e).width();
 			});
 
-			if (globalThis.gallerySliderCurrentPosition > 0)
+			if (globalThis.gallerySliderCurrentPosition < 0)
 				globalThis.gallerySliderCurrentPosition += 300;
 			$(".gallery-slider-inner").css({left: globalThis.gallerySliderCurrentPosition});
 		});
