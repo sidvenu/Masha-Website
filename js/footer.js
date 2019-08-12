c3b5cf9a900f73178451cdf1c3647bbfa0f0bd7e
Shyam <m.shyam.tnj@gmail.com>
2019-08-12 15:11:17 +0530

    update todo change footer

--- js/footer.js
+++ js/footer.js
@@ -2,31 +2,24 @@ document.write(`
     <section id="footerZ" style="margin-top:45px;">
 		<div class="">
 
-			<div class="row counters">
+			//
 
-				<div class="col-lg-4 col-6 text-center m-t-left">
-
-					<h6 style="margin-top:15px;"><b>About Us</b></h6><br />
-					<p style="margin-top:-27px;">Masha Support.</p>
+			<div class="row">
+				<div class="col-sm-12 col-lg-4">
+					<img src="/img/branding.png" style="width: 100px; height: 100px;"/>
 				</div>
-
-				<div class="col-lg-4 col-6 text-center">
-
-					<h6 style="margin-top:15px;"><b>Help</b></h6>
-					<p style="margin-top:-6px;">Privacy Policy</p>
-					<p>FAQ</p>
+				<div class="col-sm-12 col-lg-4">
+					<h6>Masha<b>art</b></h6>
+					<a href="#">Gallery</a><br />
+					<a href="#">Artists</a><br />
+					<a href="#">Showcase</a><br />
+					<a href="#">Masha Support</a><br />
+					<a href="#">Contact US</a><br />
+				</div>
+				<div class="col-sm-12 col-lg-4">
 
 				</div>
-
-				<div class="col-lg-4 col-12 text-center m-t-right">
-
-					<h6 style="margin-top:15px;"><b>Contact</b></h6>
-					<p style="margin-top:-6px;">Facebook</p>
-                    <p>Instagram</p>
-
-                </div>
-                
-            </div>
+			</div>
 
             <br />
 
@@ -39,3 +32,30 @@ document.write(`
 		</div>
 	</section>
 `);
+
+
+// <div class="row counters">
+
+// <div class="col-lg-4 col-6 text-center m-t-left">
+
+// 	<h6 style="margin-top:15px;"><b>About Us</b></h6><br />
+// 	<p style="margin-top:-27px;">Masha Support.</p>
+// </div>
+
+// <div class="col-lg-4 col-6 text-center">
+
+// 	<h6 style="margin-top:15px;"><b>Help</b></h6>
+// 	<p style="margin-top:-6px;">Privacy Policy</p>
+// 	<p>FAQ</p>
+
+// </div>
+
+// <div class="col-lg-4 col-12 text-center m-t-right">
+
+// 	<h6 style="margin-top:15px;"><b>Contact</b></h6>
+// 	<p style="margin-top:-6px;">Facebook</p>
+// 	<p>Instagram</p>
+
+// </div>
+
+// </div>
