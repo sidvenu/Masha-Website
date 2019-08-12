8e93d0f8852d72786966856840868e6a27f52fbf
Shyam <m.shyam.tnj@gmail.com>
2019-08-12 18:46:10 +0530

    edit footer content

--- js/footer.js
+++ js/footer.js
@@ -23,9 +23,9 @@ document.write(`
 				</div>
 				<div class="col-sm-12 col-lg-3">
 					<h4><a href="#">Masha</a></h4>
-					<a href="masha.html" class="sublink">Support</a><br />
 					<a href="about.html" class="sublink">About</a><br />
 					<a href="inmedia.html" class="sublink">In Media</a><br /><br />
+					<h4><a href="masha.html">Masha Support</a></h4> <br />
 					<h4>Social</h4>
 					<a href="facebook.com" class="sublink"><i class="fa fa-facebook"></i> Facebook</a><br />
 					<a href="instagram.com" class="sublink"><i class="fa fa-instagram"></i> Instagram</a><br />
