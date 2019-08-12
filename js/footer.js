f230ff7dd4978f5dbc77b1dd3904f427723a65d1
Shyam <m.shyam.tnj@gmail.com>
2019-08-12 18:24:51 +0530

    restructure footer

--- js/footer.js
+++ js/footer.js
@@ -2,60 +2,38 @@ document.write(`
     <section id="footerZ" style="margin-top:45px;">
 		<div class="">
 			<div class="row">
-				<div class="col-sm-12 col-lg-4 text-center">
+				<div class="col-sm-12 col-lg-3 text-center">
 					<img src="/img/branding.png" style="width: 200px; height: 200px;"/>
 					<h2>Masha Art</h2>
 				</div>
-				<div class="col-sm-12 col-lg-8">
-					<table style="border: none; ">
-						<tr>
-							<td><h4>Gallery</h4></td>
-							<td><h4><a href="#">Masha</a></h4></td>
-							<td><h4>Contact</h4></td>
-						</tr>
-						<tr>
-							<td>
-								<a href="Gallery.html?type=paintings" class="sublink">Paintings</a><br />
-								<a href="Gallery.html?type=shawls" class="sublink">Shawls</a><br />
-								<a href="Gallery.html?type=sculptures" class="sublink">Sculptures</a><br />
-								<a href="Gallery.html?type=carpets" class="sublink">Carptets</a><br />
-							</td>
-							<td>
-								<a href="masha.html" class="sublink">Support</a><br />
-								<a href="about.html" class="sublink">About</a><br />
-								<a href="inmedia.html" class="sublink">In Media</a><br /><br />
-							</td>
-							<td>
-								<a class="sublink" href="tel:+918948799799"><i class="glyphicon glyphicon-earphone"></i> +91-8948799799</a><br />
-								<a class="sublink" href="tel:+919999636199"><i class="glyphicon glyphicon-earphone"></i> +91-9999636199</a><br />
-								<p>#212/214, M3M Cosmopolitan<br />
-								Sector 66; Gurugram, Haryana 12210</p>
-								<a class="sublink" href="https://maps.app.goo.gl/zH765GrPdPjKEuLd7"><i class="glyphicon glyphicon-map-marker"></i> Find us on Google Maps</a><br />
-							</td>
-						</tr>
-						<tr>
-							<td><h4>Artists</h4></td>
-							<td><h4>Social</h4></td>
-						</tr>
-						<tr>
-							<td>
-								<a href="Gallery.html?type=artists" class="sublink">Featured artists</a><br />
-								<a href="Gallery.html?type=artists" class="sublink">All artists</a><br />
-							</td>
-							<td></td>
-						</tr>
-						<tr>
-							<td>
-								<h4>Showcase</h4>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href="events.html?type=events" class="sublink">Upcoming events</a><br />
-								<a href="events.html?type=exhibitions" class="sublink">Exhibitions</a><br />
-							</td>
-						</tr>
-					</table>
+
+				<div class="col-sm-12 col-lg-3">
+					<h4>Gallery</h4>
+					<a href="Gallery.html?type=paintings" class="sublink">Paintings</a><br />
+					<a href="Gallery.html?type=shawls" class="sublink">Shawls</a><br />
+					<a href="Gallery.html?type=sculptures" class="sublink">Sculptures</a><br />
+					<a href="Gallery.html?type=carpets" class="sublink">Carptets</a><br />
+					<h4>Artists</h4>
+					<a href="Gallery.html?type=artists" class="sublink">Featured artists</a><br />
+					<a href="Gallery.html?type=artists" class="sublink">All artists</a><br />
+					<h4>Showcase</h4>
+					<a href="events.html?type=events" class="sublink">Upcoming events</a><br />
+					<a href="events.html?type=exhibitions" class="sublink">Exhibitions</a><br />
+				</div>
+				<div class="col-sm-12 col-lg-3">
+					<h4><a href="#">Masha</a></h4>
+					<a href="masha.html" class="sublink">Support</a><br />
+					<a href="about.html" class="sublink">About</a><br />
+					<a href="inmedia.html" class="sublink">In Media</a><br /><br />
+					<h4>Social</h4>
+				</div>
+				<div class="col-sm-12 col-lg-3">
+					<h4>Contact</h4>
+					<a class="sublink" href="tel:+918948799799"><i class="glyphicon glyphicon-earphone"></i> +91-8948799799</a><br />
+					<a class="sublink" href="tel:+919999636199"><i class="glyphicon glyphicon-earphone"></i> +91-9999636199</a><br />
+					<p>#212/214, M3M Cosmopolitan<br />
+					Sector 66; Gurugram, Haryana 12210</p>
+					<a class="sublink" href="https://maps.app.goo.gl/zH765GrPdPjKEuLd7"><i class="glyphicon glyphicon-map-marker"></i> Find us on Google Maps</a><br />
 				</div>
 			</div>
 
