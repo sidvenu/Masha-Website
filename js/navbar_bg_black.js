429c987183aa7f14259547d49c751e1a75b1a18b
Shyam <m.shyam.tnj@gmail.com>
2019-08-23 23:18:08 +0530

    add header to index

--- js/navbar_bg_black.js
+++ js/navbar_bg_black.js
@@ -4,6 +4,7 @@ document.write(`
 
 			<div id="logo" class="pull-left">
 				<a href="index.html"><img src="img/FinalLogoEditWhite@2x.png" alt="" title=""/></img></a>
+				<span class="title"><b>masha</b> <span style="color: orange">art</span></span>
 			</div>
 
 			<nav id="nav-menu-container">
